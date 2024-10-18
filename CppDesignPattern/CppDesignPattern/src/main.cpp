d054a45c20d17f5488f55b9a736e8d981fac8d16
DevH <lequochaidevh@gmail.com>
2024-10-19 04:57:11 +0700

    add Decorator and Command

--- CppDesignPattern/CppDesignPattern/src/main.cpp
+++ CppDesignPattern/CppDesignPattern/src/main.cpp
@@ -4,9 +4,10 @@
 #include "Singleton.h"
 #include "FactoryMethod.h"
 #include "AbstractFactory.h"
-
+#include "Decorator.h"
+#include "Command.h"
 #include <iostream>
 int main() {
-	nsFactoryMethod::programTest();
+	nsCommand::programTest();
 	return 0;
 }
